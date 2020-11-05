e8511aea4b860b9e45a4603e40eced2f0809a61f
Araf020 <Araf020@users.noreply.github.com>
2020-11-05 23:26:21 +0600

    SALE_PRODUCT PAGE IS DONE

--- daraz/urls.py
+++ daraz/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('',views.list_jobs, name='test'),
-    path('signup/', views.signup, name='signup'),
+    path('home/signup/', views.signup, name='signup'),
     path('login/', views.user_login, name='login'),
     path('home/', views.home, name = 'home'),
     path('home/sell/', views.sell, name='sell'),
