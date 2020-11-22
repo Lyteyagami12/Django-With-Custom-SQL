57f3393adc0bee80f81f6ecc586ccc744c4b95df
Araf020 <Araf020@users.noreply.github.com>
2020-11-22 23:47:17 +0600

    Products added

--- djangoProject6/settings.py
+++ djangoProject6/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
         'NAME': 'ORCL',
-        'USER': 'DARAZ1',
-        'PASSWORD': '1234',
+        'USER': 'Daraz',
+        'PASSWORD': 'daraz123',
         'HOST': 'localhost',
         'PORT': '1521',
     }
